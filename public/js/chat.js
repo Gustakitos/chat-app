a464053a5303d7e20ff639302e6750e84fcbd400
Luis Gustavo <gustakitos@Gustavos-Macbook.local>
2019-03-25 00:44:12 -0300

    refactoring DOM manipulation

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,11 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $input = $messageForm.querySelector('input')
+const $button = $messageForm.querySelector('button')
+const $locationBtn = document.querySelector('#send-location')
+
 socket.on('connection', (message) => {
     console.log(message)
 })
@@ -8,10 +14,43 @@ socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $button.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        $button.removeAttribute('disabled')
+        $input.value = ''
+        $input.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
 })
+
+$locationBtn.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocalization is not supported by your browser.')
+    }
+
+    $locationBtn.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $locationBtn.removeAttribute('disabled')
+            console.log('Location shared!')
+        })
+    })
+
+    
+})
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,9 +17,26 @@ io.on('connection', (socket) => {
     console.log('new web socket connection')
 
     socket.emit('connection', 'Welcome to the App!')
+    socket.broadcast.emit('message', 'A new user joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
